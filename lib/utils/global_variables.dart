2482422a092fde0e6634cdc86e7d34d810a31fd9
Quek Yao Jing <yaojingquek@gmail.com>
2024-01-07 10:31:34 +0800

    Add search user feature

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/screen/add_post_screen.dart';
 import 'package:instagram_flutter/screen/feed_screen.dart';
+import 'package:instagram_flutter/screen/search_screen.dart';
 
 const webScreenSize = 600;
 
 const homeScreenItems = [
   FeedScreen(),
-  Text('search'),
+  SearchScreen(),
   AddPostScreen(),
   Text('notice'),
   Text('profile'),
